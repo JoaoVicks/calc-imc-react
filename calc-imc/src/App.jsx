cbdcf61181757d5ecf70be00464454c395a7e9a1
João Vitorio <joaopedronm53@gmail.com>
2024-05-11 22:33:45 -0300

    terceiro commit

--- calc-imc/src/App.jsx
+++ calc-imc/src/App.jsx
@@ -1,32 +1,38 @@
 import { useState } from 'react'
 import ImcCalc from '/src/components/ImcCalc.jsx'
+import ImcTable from '/src/components/ImcTable.jsx'
 import './App.css'
 
 function App() {
 
   const [imc, setImc] = useState("");
-  
-  const calculateImc = (height, weight) => {
 
-    console.log('sucesso');
+  const calculateImc = (height, weight) => {
 
     if (!height || !weight) return;
 
     const updateHeight = +height.replace(',', '.');
     const updateWeight = +weight.replace(',', '.');
 
-    const imcResult = updateWeight / (updateHeight ** 2);
+    let imcResult = (updateWeight / (updateHeight ** 2)).toFixed(2);
 
+  
     console.log(imcResult);
-    setImc(imcResult)
+
+    setImc(imcResult);
+
+    
+
   }
+  
+
 
 
 
   return (
     <>
       <div className="container">
-        {!imc ? (<ImcCalc calcImc={calculateImc} />) : (<ImcTable />)}
+        {!imc ? (<ImcCalc calcImc={calculateImc} />) : (<ImcTable imc = {imc} />)}
       </div>
     </>
   )
